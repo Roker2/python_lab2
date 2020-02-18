8368d9ab3d8e4027cd0c2ad91de547cdf87fb57f
Roker2 <dimaminko2000@gmail.com>
2020-02-18 21:14:26 +0300

    Add generate_file

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import argparse
+import random
+
+
+def generate_file():
+    with open('numbers.txt', 'w') as f:
+        f.writelines('{}\n'.format(random.randint(-1000000, 1000000)) for _ in range(500000000))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--type', type=str, help='Type program: one, two, three, four, five')
+    parser.add_argument('--type', type=str, help='Type program: one, two, three, four, five, generate_file')
