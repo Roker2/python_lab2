69222e5caf0a70c336220ca6ccb48265eb3e88c5
Roker2 <dimaminko2000@gmail.com>
2020-02-20 21:20:53 +0300

    to_json: Add serialization with key

--- to_json.py
+++ to_json.py
@@ -13,3 +13,20 @@ def obj_to_json(obj):
             string += obj_to_json(obj[i]) + ",\n"
         string += obj_to_json(obj[len(obj) - 1]) + "\n]"
         return string
+
+
+def obj_with_to_json(obj, key):
+    print(str(type(obj)) + key)
+    if (str(type(obj)) == '<class \'int\'>') | (str(type(obj)) == '<class \'float\'>') \
+            | (str(type(obj)) == '<class \'long\'>'):
+        return "\"" + key + "\": " + str(obj)
+    if str(type(obj)) == '<class \'str\'>':
+        return "\"" + key + "\": \"" + obj + "\""
+    if str(type(obj)) == '<class \'bool\'>':
+        return "\"" + key + "\": " + str(obj).lower()
+    if str(type(obj)) == '<class \'list\'>':
+        string = "\"" + key + "\": [\n"
+        for i in range(0, len(obj) - 1):
+            string += obj_to_json(obj[i]) + ",\n"
+        string += obj_to_json(obj[len(obj) - 1]) + "\n]"
+        return string
