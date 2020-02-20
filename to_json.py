f0e4ec98272fb0901c8058527a2f5baf9b06a3a1
Roker2 <dimaminko2000@gmail.com>
2020-02-20 20:33:43 +0300

    to_json: Add str support

--- to_json.py
+++ to_json.py
@@ -2,3 +2,5 @@ def obj_to_json(obj):
     if (str(type(obj)) == '<class \'int\'>') | (str(type(obj)) == '<class \'float\'>') \
             | (str(type(obj)) == '<class \'long\'>'):
         return "\"number\": " + str(obj)
+    if str(type(obj)) == '<class \'str\'>':
+        return "\"string\": \"" + obj + "\""
