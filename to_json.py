1d51fdf60a5de06024ccbdebb29015f58bf5bbc7
Roker2 <dimaminko2000@gmail.com>
2020-02-20 20:19:08 +0300

    to_json: Add obj_to_json which support int, float and long

---
+++ to_json.py
@@ -0,0 +1,4 @@
+def obj_to_json(obj):
+    if (str(type(obj)) == '<class \'int\'>') | (str(type(obj)) == '<class \'float\'>') \
+            | (str(type(obj)) == '<class \'long\'>'):
+        return "\"number\": " + str(obj)
