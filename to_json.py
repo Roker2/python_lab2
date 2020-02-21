77835474096a5a6744853a1e2c6470c52816b547
Roker2 <dimaminko2000@gmail.com>
2020-02-21 09:46:01 +0300

    to_json: Optimize
    Move some code to common functions
    Remove logging

--- to_json.py
+++ to_json.py
@@ -1,46 +1,53 @@
 def obj_to_json(obj):
-    print(type(obj))
+    # print(type(obj))
     if (str(type(obj)) == '<class \'int\'>') | (str(type(obj)) == '<class \'float\'>') \
             | (str(type(obj)) == '<class \'long\'>'):
         return "\"number\": " + str(obj)
     if str(type(obj)) == '<class \'str\'>':
-        return "\"string\": \"" + obj + "\""
+        return str_to_json(obj)
     if str(type(obj)) == '<class \'bool\'>':
-        return "\"boolean\": " + str(obj).lower()
+        return bool_to_json(obj)
     if str(type(obj)) == '<class \'list\'>':
-        string = "\"array\": [\n"
-        for i in range(0, len(obj) - 1):
-            string += obj_to_json(obj[i]) + ",\n"
-        string += obj_to_json(obj[len(obj) - 1]) + "\n]"
-        return string
+        return list_to_json(obj)
     if str(type(obj)) == '<class \'dict\'>':
-        string = "\"object\": {\n"
-        for key in dict(obj).keys():
-            string += obj_with_to_json(obj[key], key) + ",\n"
-        string = string[:(len(string) - 2)] + string[(len(string) - 2):].replace(",\n", "")
-        string += "\n}"
-        return string
+        return dict_to_json(obj)
 
 
 def obj_with_to_json(obj, key):
-    print(str(type(obj)) + key)
+    # print(str(type(obj)) + key)
     if (str(type(obj)) == '<class \'int\'>') | (str(type(obj)) == '<class \'float\'>') \
             | (str(type(obj)) == '<class \'long\'>'):
         return "\"" + key + "\": " + str(obj)
     if str(type(obj)) == '<class \'str\'>':
-        return "\"" + key + "\": \"" + obj + "\""
+        return str_to_json(obj, key)
     if str(type(obj)) == '<class \'bool\'>':
-        return "\"" + key + "\": " + str(obj).lower()
+        return bool_to_json(obj, key)
     if str(type(obj)) == '<class \'list\'>':
-        string = "\"" + key + "\": [\n"
-        for i in range(0, len(obj) - 1):
-            string += obj_to_json(obj[i]) + ",\n"
-        string += obj_to_json(obj[len(obj) - 1]) + "\n]"
-        return string
+        return list_to_json(obj, key)
     if str(type(obj)) == '<class \'dict\'>':
-        string = "\"" + key + "\": {\n"
-        for key in dict(obj).keys():
-            string += obj_with_to_json(obj[key], key) + ",\n"
-        string = string[:(len(string) - 2)] + string[(len(string) - 2):].replace(",\n", "")
-        string += "\n}"
-        return string
+        return dict_to_json(obj, key)
+
+
+def str_to_json(obj, key="string"):
+    return "\"" + key + "\": \"" + obj + "\""
+
+
+def bool_to_json(obj, key="boolean"):
+    return "\"" + key + "\": " + str(obj).lower()
+
+
+def list_to_json(obj, key="array"):
+    string = "\"" + key + "\": [\n"
+    for i in range(0, len(obj) - 1):
+        string += obj_to_json(obj[i]) + ",\n"
+    string += obj_to_json(obj[len(obj) - 1]) + "\n]"
+    return string
+
+
+def dict_to_json(obj, key="object"):
+    string = "\"" + key + "\": {\n"
+    for key in dict(obj).keys():
+        string += obj_with_to_json(obj[key], key) + ",\n"
+    string = string[:(len(string) - 2)] + string[(len(string) - 2):].replace(",\n", "")
+    string += "\n}"
+    return string
