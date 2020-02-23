ceb0b26c53cea593d0591593b45f2df25328d520
Roker2 <dimaminko2000@gmail.com>
2020-02-23 17:56:11 +0300

    sorting_by_merges: Sort bigger massives

--- sorting_by_merges.py
+++ sorting_by_merges.py
@@ -2,34 +2,37 @@ import file_work
 
 
 def sort_merge(filename):
-    i = 1
     n = 1
     count = file_work.count_lines_in_file(filename)
     remainder = count % n
     left_index = 0
     right_index = 0
-    file = open("sorted.txt", "w")
-    while i + 1 <= count:
-        """
-        print("i=" + str(i))
-        print("i + right_index + n=" + str(i + right_index + n) + ": " + file_work.read_n_line(filename, i + right_index + n))
-        print("i + left_index=" + str(i + left_index) + ": " + file_work.read_n_line(filename, i + left_index) + '\n')
-        """
-        for _ in range(n * 2):
-            if (left_index < n) & (right_index < n):
-                if int(file_work.read_n_line(filename, i + left_index)) <= int(file_work.read_n_line(filename, i + right_index + n)):
-                    file.write(file_work.read_n_line(filename, i + left_index))
-                    left_index += 1
-                else:
+    while n <= count // 2:
+        i = 1
+        file = open("sorted" + str(n) + ".txt", "w")
+        while i + 1 <= count:
+            """
+            print("i=" + str(i))
+            print("i + right_index + n=" + str(i + right_index + n) + ": " + file_work.read_n_line(filename, i + right_index + n))
+            print("i + left_index=" + str(i + left_index) + ": " + file_work.read_n_line(filename, i + left_index) + '\n')
+            """
+            for _ in range(n * 2):
+                if (left_index < n) & (right_index < n):
+                    if int(file_work.read_n_line(filename, i + left_index)) <= int(file_work.read_n_line(filename, i + right_index + n)):
+                        file.write(file_work.read_n_line(filename, i + left_index))
+                        left_index += 1
+                    else:
+                        file.write(file_work.read_n_line(filename, i + right_index + n))
+                        right_index += 1
+                elif left_index == n:
                     file.write(file_work.read_n_line(filename, i + right_index + n))
                     right_index += 1
-            elif left_index == n:
-                file.write(file_work.read_n_line(filename, i + right_index + n))
-                right_index += 1
-            elif right_index == n:
-                file.write(file_work.read_n_line(filename, i + left_index))
-                left_index += 1
-        i += n * 2
-        left_index = 0
-        right_index = 0
-    file.close()
+                elif right_index == n:
+                    file.write(file_work.read_n_line(filename, i + left_index))
+                    left_index += 1
+            i += n * 2
+            left_index = 0
+            right_index = 0
+        file.close()
+        filename = file.name
+        n *= 2
