37517f821ad7ecfbddffbc3e56ca792d02b2630f
Roker2 <dimaminko2000@gmail.com>
2020-02-29 13:58:46 +0300

    main: Add --generate_file option

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ def plus_and_pow(a, b, n):
     return math.pow(a + b, n)
 
 
-def generate_file(quantity=500000000):
+def generate_file(quantity='500000000'):
     with open('numbers.txt', 'w') as f:
-        f.writelines('{}\n'.format(random.randint(-1000000, 1000000)) for _ in range(quantity))
+        f.writelines('{}\n'.format(random.randint(-1000000, 1000000)) for _ in range(int(quantity)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--type', type=str, help='Type program: one, two, three, four, five, generate_file')
+    parser.add_argument('-g', '--generate_file', type= generate_file)
 
     if vars(parser.parse_args())['type'] == 'generate_file':
-        generate_file(50)
+        generate_file("50")
     if vars(parser.parse_args())['type'] == 'two':
         MyDict = {"one": 1, "second": "LOL", "massiv": [555, 666], "Dict2": {"heh": 555, "kek": "lol"}, "bool": True}
         print(MyDict)
