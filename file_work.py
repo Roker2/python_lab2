f912ef466f93f6816d00d3641049da1ef286f258
Roker2 <dimaminko2000@gmail.com>
2020-03-14 13:50:37 +0300

    file_work: Fixes

--- file_work.py
+++ file_work.py
@@ -6,8 +6,8 @@ def read_n_line(filename, n):
 
 
 def count_lines_in_file(filename, n=1):
-    file = open(filename)
-    summa = sum(1 for line in file) - n + 1
+    with open(filename, "r") as file:
+        summa = sum(1 for line in file) - n + 1
     if summa < 0:
         return -1
     else:
